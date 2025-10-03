2a2de9f9a18bb910009d86e3f17dfba97c454f19
icg <i.castrogarciafraile@gmail.com>
2025-10-03 19:42:15 +0200

    cambios a main

--- windows/main_window.py
+++ windows/main_window.py
@@ -1,6 +1,28 @@
-from PyQt5 import QWidget
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLineEdit
 
 class mainWindow(QWidget):
     def __init__(self):
-        super.__init__()
+        super().__init__()
+
+        self.setWindowTitle("Gestor de Incidencias con IA")
+        self.setFixedSize(800,400)
+
+        layout_principal = QVBoxLayout()
+        layout_filtros = QHBoxLayout()
+        layout_bloque_incidencias = QHBoxLayout()
+        layout_botones_incidencias = QVBoxLayout()
+
+        input_filtro = QLineEdit(self)
+        layout_filtros.addWidget(input_filtro)
+
+        layout_principal.addLayout(layout_filtros)
+        layout_principal.addLayout(layout_bloque_incidencias)
+
+
+
         
+
+
+        self.setLayout = layout_principal
+    
+
