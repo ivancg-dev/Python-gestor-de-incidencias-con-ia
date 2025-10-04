9072903c497693c14078e1aae377d1e7aeda97e0
Sergio <sergio_aperez@alumnos-santagema.es>
2025-10-04 11:00:23 +0200

    Mejora del login_window

--- windows/login_window.py
+++ windows/login_window.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 
@@ -10,7 +10,10 @@ class loginWindow(QWidget):
         self.setFixedWidth(600)
         self.setFixedHeight(400)
 
-        # Crear el título con estilo y alinearlo al centro
+        '''
+        Título
+        '''
+
         label_1 = QLabel("Iniciar sesión")
         titulo_fuente = QFont()
         titulo_fuente.setPointSize(24)
@@ -18,45 +21,62 @@ class loginWindow(QWidget):
         label_1.setFont(titulo_fuente)
         label_1.setAlignment(Qt.AlignCenter)
 
-        label_3 = QLabel("tercero label")
-        label_4 = QLabel("Usuario:")
-        label_5 = QLabel("Contraseña:")
-        boton_login = QPushButton("Iniciar sesión")
-        boton_singup = QPushButton("Iniciar sesión")
+        '''
+        Labels
+        '''
 
+        label_2 = QLabel("Usuario:")
+        label_3 = QLabel("Contraseña:")
 
+        '''
+        Campos de texto más grandes
+        '''
 
-        # Campos de texto
         input_usuario = QLineEdit()
-        input_contrasena = QLineEdit()
-        input_contrasena.setEchoMode(QLineEdit.Password)  # Oculta el texto
+        input_usuario.setFixedHeight(40)
+        input_usuario.setMinimumWidth(300)
 
+        input_contrasena = QLineEdit()
+        '''Hacemos que en el QLineEdit no se vea lo que se escribe, solo se ven puntos'''
+        input_contrasena.setEchoMode(QLineEdit.Password)
+        input_contrasena.setFixedHeight(40)
+        input_contrasena.setMinimumWidth(300)
+
+        '''Botones más grandes'''
+        boton_login = QPushButton("Iniciar sesión")
+        boton_login.setFixedSize(150, 40)
+
+        boton_singup = QPushButton("Registrarse")
+        boton_singup.setFixedSize(150, 40)
+
+        '''Layouts'''
         layout_principal = QVBoxLayout()
         layout_secundario = QVBoxLayout()
-        layout_secundario_1 = QVBoxLayout()
-        layout_secundario_2 = QVBoxLayout()
-        layout_secundario_1_1 = QVBoxLayout()
-        layout_secundario_1_2 = QVBoxLayout()
-        layout_secundario_2_1 = QVBoxLayout()
-        layout_secundario_2_2 = QVBoxLayout()
-        layout_terciario = QVBoxLayout()
+        layout_secundario_1_1 = QHBoxLayout()
+        layout_secundario_2_1 = QHBoxLayout()
+        layout_botones = QHBoxLayout()
 
         layout_principal.addWidget(label_1)
-        layout_terciario.addWidget(label_3)
+        '''addSpacing son espacio entre título y campos'''
+        layout_principal.addSpacing(40)  
 
-        layout_secundario_1_1.addWidget(label_4)
-        layout_secundario_1_2.addWidget(input_usuario)
-        layout_secundario_2_1.addWidget(label_5)
-        layout_secundario_2_2.addWidget(input_contrasena)
-        layout_terciario.addWidget(boton_login)
+
+        layout_secundario_1_1.addWidget(label_2)
+        layout_secundario_1_1.addWidget(input_usuario)
+        layout_secundario_2_1.addWidget(label_3)
+        layout_secundario_2_1.addWidget(input_contrasena)
+
+        layout_botones.addWidget(boton_login)
+        layout_botones.addStretch()
+        '''addStrech lleva el segundo botón al otro extremo'''
+        layout_botones.addWidget(boton_singup)
+
+        layout_secundario.addLayout(layout_secundario_1_1)
+        layout_secundario.addSpacing(20)
+        layout_secundario.addLayout(layout_secundario_2_1)
 
         layout_principal.addLayout(layout_secundario)
-        layout_principal.addLayout(layout_terciario)
-        layout_secundario.addLayout(layout_secundario_1)
-        layout_secundario.addLayout(layout_secundario_2)
-        layout_secundario.addLayout(layout_secundario_1_1)
-        layout_secundario.addLayout(layout_secundario_1_2)
-        layout_secundario.addLayout(layout_secundario_2_1)
-        layout_secundario.addLayout(layout_secundario_2_2)
+        layout_principal.addStretch()
+        layout_principal.addLayout(layout_botones)
 
         self.setLayout(layout_principal)
